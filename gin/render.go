e92e0a7bfaa114cafe48641403497aa3b842c0f9
kpacha <kpacha666@gmail.com>
2019-07-13 17:49:41 +0200

    gin xml render added

---
+++ gin/render.go
@@ -0,0 +1,19 @@
+package gin
+
+import (
+	"github.com/clbanning/mxj"
+	"github.com/devopsfaith/krakend/proxy"
+	"github.com/gin-gonic/gin"
+)
+
+// Render marshals the proxy response and passes the resulting xml to the response writer
+func Render(c *gin.Context, response *proxy.Response) {
+	status := c.Writer.Status()
+	if response == nil {
+		c.XML(status, nil)
+		return
+	}
+	mv := mxj.Map(response.Data)
+	c.Header("Content-Type", gin.MIMEXML)
+	mv.XmlWriter(c.Writer)
+}
---
+++ gin/render_test.go
@@ -0,0 +1,55 @@
+package gin
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/devopsfaith/krakend/proxy"
+	"github.com/gin-gonic/gin"
+)
+
+func TestRender(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	server := gin.New()
+	server.GET("/", func(c *gin.Context) {
+		res := &proxy.Response{
+			IsComplete: true,
+			Data: map[string]interface{}{
+				"a": map[string]interface{}{
+					"content": "supu",
+				},
+				"content": "tupu",
+				"foo":     42,
+			},
+		}
+		Render(c, res)
+	})
+
+	expected := `<doc><a><content>supu</content></a><content>tupu</content><foo>42</foo></doc>`
+
+	req, _ := http.NewRequest("GET", "http://127.0.0.1:8080/", nil)
+
+	w := httptest.NewRecorder()
+	server.ServeHTTP(w, req)
+
+	defer w.Result().Body.Close()
+
+	body, ioerr := ioutil.ReadAll(w.Result().Body)
+	if ioerr != nil {
+		t.Error("reading response body:", ioerr)
+		return
+	}
+
+	content := string(body)
+	if w.Result().Header.Get("Content-Type") != gin.MIMEXML {
+		t.Error("Content-Type error:", w.Result().Header.Get("Content-Type"))
+	}
+	if w.Result().StatusCode != http.StatusOK {
+		t.Error("Unexpected status code:", w.Result().StatusCode)
+	}
+	if content != expected {
+		t.Error("Unexpected body:", content, "expected:", expected)
+	}
+}
